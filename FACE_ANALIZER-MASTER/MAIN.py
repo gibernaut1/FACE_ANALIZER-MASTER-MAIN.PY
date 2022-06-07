597a81cd3bc762c1eb9c09a8951a3a0079e862b4
gibernaut1 <69781727+gibernaut1@users.noreply.github.com>
2022-06-07 16:13:04 +0300

    Add files via upload

---
+++ FACE_ANALIZER-MASTER/MAIN.py
@@ -0,0 +1,75 @@
+#адрес d:\Pytonprogrammi\FACE_ANALIZER-MASTER\MAIN.PY
+#Программа содержит 3 функции:
+#1.)def face_verify(img_1,img_2):-функция ,которая сравнивает два файла img_1 и img_2- и выводит сообщение-проверка пройдена,если лица на фотографии одинаковые лица
+# и сообщение -нарушитель задержать!,  если проверка не пройдена.
+#2.)функция face_recogn():-,берет лицо из папки "faces\jim.jpg" и сравнивается с лицами из папки Emilia
+#3.)face_analyze функция которая берет лицо из папки faces и определяет пол,возраст,настроение,расу.
+from deepface import DeepFace
+import json
+
+
+def face_verify(img_1, img_2):
+    try:
+        result_dict = DeepFace.verify(img1_path=img_1, img2_path=img_2,model_name='Facenet512')
+        
+        with open('result.json', 'w') as file:
+            json.dump(result_dict, file, indent=4, ensure_ascii=False)
+        
+        # return result_dict
+        
+        if result_dict.get('verified'):
+            return 'Проверка пройдена. Пропустить.'
+        return 'Нарушитель! Задержать!!!'
+    
+    except Exception as _ex:
+        return _ex
+    
+    
+def face_recogn():
+    try:
+        result = DeepFace.find(img_path='faces/jim.jpg', db_path='Emilia')
+        result = result.values.tolist()
+
+        print ('RESULT=',result)         
+        return result
+    
+    
+    except Exception as _ex:
+        return _ex
+    
+
+def face_analyze():
+    try:
+        # result_dict = DeepFace.analyze(img_path='faces/jim.jpg', actions=['age', 'gender', 'race', 'emotion'])
+        # result_dict = DeepFace.analyze(img_path='faces/adr.jpg', actions=['age', 'gender', 'race', 'emotion'])
+        result_dict = DeepFace.analyze(img_path='faces/Nastya.jpg', actions=['age', 'gender', 'race', 'emotion'])
+        
+        with open('face_analyze.json', 'w') as file:
+            json.dump(result_dict, file, indent=4, ensure_ascii=False)
+            
+        print(f'[+] Age: {result_dict.get("age")}')
+        print(f'[+] Gender: {result_dict.get("gender")}')
+        print('[+] Race:')
+        
+        for k, v in result_dict.get('race').items():
+            print(f'{k} - {round(v, 2)}%')
+            
+        print('[+] Emotions:')
+        
+        for k, v in result_dict.get('emotion').items():
+            print(f'{k} - {round(v, 2)}%')
+            
+        # return result_dict
+
+    except Exception as _ex:
+        return _ex
+
+    
+def main():
+    print(face_verify(img_1='faces/em1.jpg', img_2='faces/em2.jpg'))
+    print(face_recogn())
+    face_analyze()
+    
+    
+if __name__ == '__main__':
+    main()
